eb4592c2a8e44bdd43590c99a50a426e69e7f4fc
Sagy Blecher <sagyblecher@Sagys-MacBook-Pro.local>
2025-02-26 14:06:50 +0200

    Import file example assets/hello.js.

--- extensions/bundle-cart/assets/hello.js
+++ extensions/bundle-cart/assets/hello.js
@@ -3,5 +3,10 @@ function printHelloWorld() {
   console.log("✅ Hello, World!");
 }
 
+function printGoodbyeWorld() {
+  console.log("✅ Goodbye, World!");
+}
+
 // ✅ Expose the function globally so it can be used in the browser
 window.printHelloWorld = printHelloWorld;
+window.printGoodbyeWorld = printGoodbyeWorld;
